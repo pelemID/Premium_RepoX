d2419be5df1be9a45e658d3363468cce360ada22
pelemID <kodelz@gmail.com>
2026-09-16 16:20:33 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,16 @@
 rootProject.name = "CloudstreamPlugins"
 
 // This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
-val disabled = listOf<String>()
+// val disabled = listOf<String>()
+val disabled = listOf(
+    "DramaBox",
+    "Eporner",
+    "FreeReels",
+    "Pornhub",
+    "ReelShort",
+    "Xhamster"
+)
+
 
 File(rootDir, ".").eachDir { dir ->
     if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
